218154958badb9f7ec92151bb3791a2fd9bccfad
Edward A Maxedon <edadma@gmail.com>
2018-03-05 22:19:45 -0500

    start coding simple textbuffer

--- src/main/scala/TextBuffer.scala
+++ src/main/scala/TextBuffer.scala
@@ -1,30 +1,74 @@
+//@
 package xyz.hyperreal.text
 
-import java.awt.Color
-import java.awt.font.GlyphVector
+import java.awt.{Color, Font}
+import java.awt.font.{FontRenderContext, GlyphVector}
 
 import collection.mutable.ArrayBuffer
 
 
-class TextBuffer {
+class TextBuffer( font: Font, frc: FontRenderContext ) {
 
   private val lines = new ArrayBuffer[Line]
 
-  private class Line {
-    val chars = new ArrayBuffer[Char]
+  lines += blankLine
 
-    val styles = new ArrayBuffer[Style]
-
-    val runs = new ArrayBuffer[Run]
-  }
+  private case class Line( chars: ArrayBuffer[Char]/*, styles: ArrayBuffer[Style]*/, runs: ArrayBuffer[GlyphVector] )
 
   private case class Style( length: Int, color: Color, italic: Boolean, bold: Boolean )
 
-  private case class Run( length: Int, glyphs: GlyphVector, color: Color, italic: Boolean, bold: Boolean )
+  def show: Unit = {
 
-  def insert( c: Char, row: Int, col: Int ): Unit = {
+    for (l <- lines) {
+      println( l.chars )
+      println( l.runs )
+    }
 
   }
+
+  private def blankLine = Line( new ArrayBuffer[Char], new ArrayBuffer[GlyphVector] )
+
+  private def check( row: Int, col: Int ) {
+    require( row >= 0, "negative row" )
+    require( row < lines.length, "row is beyond last line" )
+    require( col >= 0, "negative column" )
+    require( col <= lines(row).chars.length, "column is beyond end of line" )
+  }
+
+  def eol( row: Int, col: Int ): Boolean = col == lines(row).chars.length
+
+  def insert( c: Char, row: Int, col: Int ): Unit = {
+    check( row, col )
+
+    val line = lines(row)
+
+    line.chars.insert( col, c )
+
+    def find: (Int, Int) = {
+      var count = 0
+      val runs = lines(row).runs.length
+
+      for (i <- 0 until runs) {
+        val len = lines(row).runs(i).getNumGlyphs
+
+        if (count <= col && col < count + len)
+          return (i, col - count)
+        else
+          count += len
+      }
+
+      (runs, 0)
+    }
+
+    find match {
+      case (run, 0) => line.runs.insert( run, font.createGlyphVector(frc, c.toString) )
+      case (run, offset) =>
+        line.runs.insert( run + 1, font.createGlyphVector(frc, c.toString) )
+        line.runs.insert( run + 2, font.createGlyphVector(frc, line.chars.view(col, line.runs(run).getNumGlyphs - offset).toArray) )
+        line.runs(run) = font.createGlyphVector(frc, line.chars.view(col - offset, col).toArray )
+    }
+  }
+
 
   def overwrite( c: Char, row: Int, col: Int ): Unit = {
 
@@ -34,15 +78,24 @@ class TextBuffer {
 
   }
 
-  def prev( row: Int, col: Int ) = {
-
+  def prev( row: Int, col: Int ): Option[(Int, Int)] = {
+    null
   }
 
-  def next( row: Int, col: Int ) = {
-
+  def next( row: Int, col: Int ): Option[(Int, Int)] = {
+    null
   }
 
   def backspace( row: Int, col: Int ): Boolean = {
+    prev( row, col ) match {
+      case None => false
+      case Some( (r, c) ) =>
+        delete( r, c )
+        true
+    }
+  }
 
+  def extract( row: Int, col: Int, len: Int ): (Int, List[GlyphVector]) = {
+    null
   }
 }
--- src/test/scala/Main.scala
+++ src/test/scala/Main.scala
@@ -1,6 +1,17 @@
 package xyz.hyperreal.text
 
+import java.awt.Font
+import java.awt.font.FontRenderContext
+import java.awt.geom.AffineTransform
+
 
 object Main extends App {
+
+  val buf = new TextBuffer( new Font("Monospaced", Font.PLAIN, 14), new FontRenderContext(new AffineTransform(), true, true) )
+
+  buf.show
+  buf.insert( 'a', 0, 0 )
+  buf.insert( 'b', 0, 1 )
+  buf.show
 
 }
